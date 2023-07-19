15df908ea43d292a0f6dc0fc7993b1246970b4d1
Dhafa <dhofaaryanda4@gmail.com>
2023-07-19 22:48:36 +0700

    creat models

--- database/migrations/2023_07_18_171438_create_user_premiums_table.php
+++ database/migrations/2023_07_18_171438_create_user_premiums_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('package_id')->constrained('packages');
             $table->foreignId('user_id')->constrained('users');
-            $table->date('end_of_subscription')
+            $table->date('end_of_subscription');
             $table->timestamps();
         });
     }
