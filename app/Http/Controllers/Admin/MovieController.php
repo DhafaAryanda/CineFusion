c3e90e02e2a264e60ea78f8223f4ba43dcb0be13
Dhafa <dhofaaryanda4@gmail.com>
2023-07-22 21:50:34 +0700

    make thumbnail store

--- app/Http/Controllers/Admin/MovieController.php
+++ app/Http/Controllers/Admin/MovieController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class MovieController extends Controller
 {
@@ -19,8 +20,31 @@ class MovieController extends Controller
 
     public function store(Request $request)
     {
-        $data = $request->all();
+        $data = $request->except('_token');
 
-        dd($data);
+        $request->validate([
+            'title' => 'required|string',
+            'small_thumbnail' => 'required|image|mimes:jpeg,jpg,png',
+            'large_thumbnail' => 'required|image|mimes:jpeg,jpg,png',
+            'trailer' => 'required|url',
+            'movie' => 'required|url',
+            'casts' => 'required|string',
+            'categories' => 'required|string',
+            'release_date' => 'required|string',
+            'about' => 'required|string',
+            'short_about' => 'required|string',
+            'duration' => 'required|string',
+            'featured' => 'required'
+        ]);
+
+        $smallThumbnail = $request->file('small_thumbnail');
+        $largeThumbnail = $request->file('large_thumbnail');
+        $originalSmallThumbnailName = Str::random(10).$smallThumbnail->getClientOriginalName();
+        $originalLargeThumbnailName = Str::random(10).$largeThumbnail->getClientOriginalName();
+
+        $smallThumbnail->storeAs('public/thumbnails/small', $originalSmallThumbnailName);
+        $largeThumbnail->storeAs('public/thumbnails/large', $originalLargeThumbnailName);
+
+        dd($originalSmallThumbnailName);
     }
 }
--- resources/views/admin/movie-create.blade.php
+++ resources/views/admin/movie-create.blade.php
@@ -8,6 +8,15 @@
   <div class="col-md-12">
 
     {{-- Alert Here --}}
+    @if ($errors->any())
+    <div class="alert alert-danger"> 
+      <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+    @endif
 
     <div class="card card-primary">
       <div class="card-header">
@@ -20,20 +29,24 @@
         <div class="card-body">
           <div class="form-group">
             <label for="title">Title</label>
-            <input type="text" class="form-control" id="title" name="title" placeholder="e.g Guardian of The Galaxy">
+            <input type="text" class="form-control" id="title" name="title" placeholder="e.g Guardian of The Galaxy" value="{{ old('title') }}">
           </div>
           <div class="form-group">
             <label for="trailer">Trailer</label>
-            <input type="text" class="form-control" id="trailer" name="trailer" placeholder="Video url">
+            <input type="text" class="form-control" id="trailer" name="trailer" placeholder="Video url" value="{{ old('trailer') }}">
+          </div>
+          <div class="form-group">
+            <label for="trailer">Movie</label>
+            <input type="text" class="form-control" id="movie" name="movie" placeholder="Video url" value="{{ old('movie') }}">
           </div>
           <div class="form-group">
             <label for="duration">Duration</label>
-            <input type="text" class="form-control" id="duration" name="duration" placeholder="1h 39m">
+            <input type="text" class="form-control" id="duration" name="duration" placeholder="1h 39m" value="{{ old('duration') }}">
           </div>
           <div class="form-group">
             <label>Date:</label>
             <div class="input-group date" id="release-date" data-target-input="nearest">
-              <input type="text" name="release_date" class="form-control datetimepicker-input" data-target="#release-date"/>
+              <input type="text" name="release_date" class="form-control datetimepicker-input" data-target="#release-date" value="{{ old('release_date') }}"/>
               <div class="input-group-append" data-target="#release-date" data-toggle="datetimepicker">
                 <div class="input-group-text"><i class="fa fa-calendar"></i></div>
               </div>
@@ -41,11 +54,11 @@
           </div>
           <div class="form-group">
             <label for="short-about">Casts</label>
-            <input type="text" class="form-control" id="short-about" name="casts" placeholder="Jackie Chan">
+            <input type="text" class="form-control" id="short-about" name="casts" placeholder="Jackie Chan" value="{{ old('casts') }}">
           </div>
           <div class="form-group">
             <label for="short-about">Categories</label>
-            <input type="text" class="form-control" id="short-about" name="categories" placeholder="Action, Fantasy">
+            <input type="text" class="form-control" id="short-about" name="categories" placeholder="Action, Fantasy" value="{{ old('categories') }}">
           </div>
           <div class="form-group">
             <label for="small-thumbnail">Small Thumbnail</label>
@@ -57,17 +70,17 @@
           </div>
           <div class="form-group">
             <label for="short-about">Short About</label>
-            <input type="text" class="form-control" id="short-about" name="short_about" placeholder="Awesome Movie">
+            <input type="text" class="form-control" id="short-about" name="short_about" placeholder="Awesome Movie" value="{{ old('short_about') }}">
           </div>
           <div class="form-group">
             <label for="short-about">About</label>
-            <input type="text" class="form-control" id="about" name="about" placeholder="Awesome Movie">
+            <input type="text" class="form-control" id="about" name="about" placeholder="Awesome Movie" value="{{ old('about') }}">
           </div>
           <div class="form-group">
             <label>Featured</label>
-            <select class="custom-select" name="featured">
-              <option value="0">No</option>
-              <option value="1">Yes</option>
+            <select class="custom-select" name="featured" value="{{ old('featured') }}">
+              <option value="0" {{ old('featured') === '0' ? "selected" : "" }} >No</option>
+              <option value="1" {{ old('featured') === '1' ? "selected" : "" }} >Yes</option>
             </select>
           </div>
         </div>
@@ -81,4 +94,12 @@
   </div>
 </div>
 
+@endsection
+
+@section('js')
+  <script>
+    $('#release-date').datetimepicker({
+      format: 'YYYY-MM-DD'
+    });
+  </script>
 @endsection
