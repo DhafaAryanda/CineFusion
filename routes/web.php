b02e30c1ae4510a3f7c785a3d28b3bd1463d25ad
Dhafa <dhofaaryanda4@gmail.com>
2023-07-29 23:42:18 +0700

    create middleware admin

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\MovieController;
 use App\Http\Controllers\Admin\TransactionController;
+use App\Http\Controllers\Admin\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +20,11 @@ use App\Http\Controllers\Admin\TransactionController;
 //     return view('welcome');
 // });
 
-Route::group(['prefix' => 'admin'], function() {
+Route::get('admin/login', [LoginController::class, 'index'])->name('admin.login');
+Route::post('admin/login', [LoginController::class, 'authenticate'])->name('admin.login.auth');
+
+
+Route::group(['prefix' => 'admin', 'middleware' => ['admin.auth']], function() {
     Route::view('/', 'admin.dashboard')->name('admin.dashboard');
 
     Route::get('transaction', [TransactionController::class, 'index'])->name('admin.transaction');
